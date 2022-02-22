09f27330cdb6068f8df1f311dbf72740dfa8ac02
nitesh172 <65884590+nitesh172@users.noreply.github.com>
2022-02-22 18:25:05 +0530

    Create User controller

---
+++ src/Controllers/user.controller.js
@@ -0,0 +1,10 @@
+const { Router } = require("express")
+
+const User = require("../Models/user.model")
+const crudController = require("./crud.controller")
+
+const router = Router()
+
+router.get("", crudController(User, "User").get)
+
+module.exports = router
