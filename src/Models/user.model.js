1c7e40de32556d48e7de85d34ca3da3d41eb3a50
nitesh172 <65884590+nitesh172@users.noreply.github.com>
2022-02-24 15:30:26 +0530

    Update bycrpt method

--- src/Models/user.model.js
+++ src/Models/user.model.js
@@ -1,64 +1,71 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 
 const userSchema = new mongoose.Schema(
-    {
-        first_name: {
-            type: String,
-            required: true,
-        },
-        surname: {
-            type: String,
-            required: true,
-        },
-        mobile: {
-            type: String,
-            unique: true,
-            required: true,
-        },
-        birthdate: {
-            type: Date,
-            required: true,
-        },
-        gender: {
-            type: String,
-            required: true,
-        },
-        post_code: {
-            type: Number,
-            required: true,
-        },
-        preferred_store: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            unique: true,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        member: {
-            type: Boolean,
-            default: false,
-        },
-        profilePic: {
-            type: String,
-            required: false,
-            default: "https://i.postimg.cc/MTw0t80t/pngegg-1.png",
-        },
-        confirmed: {
-            type: Boolean,
-            required: false,
-            default: false
-        }
+  {
+    first_name: {
+      type: String,
+      required: true,
     },
-    {
-        versionKey: false,
-        timestamps: true,
-    }
-) 
+    surname: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    birthdate: {
+      type: Date,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    post_code: {
+      type: Number,
+      required: true,
+    },
+    preferred_store: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    member: {
+      type: Boolean,
+      default: false,
+    },
+    profilePic: {
+      type: String,
+      required: false,
+      default: "https://i.postimg.cc/MTw0t80t/pngegg-1.png",
+    },
+    confirmed: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+)
 
-module.exports = mongoose.model("user", userSchema);
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next()
+  this.password = bcrypt.hashSync(this.password, 8)
+  return next()
+})
+
+module.exports = mongoose.model("user", userSchema)
