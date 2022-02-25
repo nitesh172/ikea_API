da72c7582417c68e1accd0db4deacca3fc735304
nitesh172 <65884590+nitesh172@users.noreply.github.com>
2022-02-25 22:59:24 +0530

    Update cart.controller.js

--- src/Controllers/cart.controller.js
+++ src/Controllers/cart.controller.js
@@ -9,24 +9,46 @@ router.get("", crudController(Cart, "Cart").get)
 router.get("/email=:email", async (req, res) => {
   try {
     const email = req.params.email
+    redis.get(email, async (err, value) => {
+      if (err) console.log(err)
 
-    const cart = await Cart.find({ userId: email }).lean().exec()
-
-    res.status(200).send(cart)
+      if (value) {
+        value = JSON.parse(value)
+        return res.status(201).send({ value, redis: true })
+      } else {
+        try {
+          const value = await Cart.findOne({ userId: email }).lean().exec()
+          redis.set(email, JSON.stringify(value))
+          res.status(201).send({ value, redis: false })
+        } catch (err) {
+          res.status(201).send(err.message)
+        }
+      }
+    })
   } catch (error) {
     console.log(error.message)
-    res.status(500).send(error.message)
+    return res.status(500).send(error.message)
   }
 })
 
 router.post("", async (req, res) => {
   try {
     const cart = await Cart.create(req.body)
+    redis.get("Cart", async (err, value) => {
+      if (err) console.log(err)
 
-    res.status(201).send(cart)
+      if (value) {
+        value = JSON.parse(value)
+        redis.set(key, JSON.stringify([...value, cart]))
+      } else {
+        value = await Cart.find().lean().exec()
+        redis.set(key, JSON.stringify(value))
+      }
+    })
+    return res.status(201).send(cart)
   } catch (error) {
     console.log(error.message)
-    res.status(500).send(error.message)
+    return res.status(500).send(error.message)
   }
 })
 
@@ -38,11 +60,18 @@ router.patch("/email=:email", async (req, res) => {
     })
       .lean()
       .exec()
+    redis.get(email, async (err, fetchedPost) => {
+      if (err) console.log(err.message)
 
-    res.status(201).send(cart)
+      redis.set(email, JSON.stringify(cart))
+
+      const carts = await Cart.find().lean().exec()
+      redis.set(key, JSON.stringify(carts))
+    })
+    res.status(201).send(item)
   } catch (error) {
     console.log(error.message)
-    res.status(500).send(error.message)
+    return res.status(500).send(error.message)
   }
 })
 
